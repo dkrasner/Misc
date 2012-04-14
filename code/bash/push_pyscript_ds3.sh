140f6b2374cc37ed68fdb07f69b815ee08d0da2d
dkrasner <daniel@math.columbia.edu>
2012-04-14 17:38:08 -0400

    removed some files

--- code/bash/push_pyscript_ds3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat $1 | ssh daniel@ec2-184-72-234-254.compute-1.amazonaws.com "cat> $1 ;scp $1 ssh ec2-50-17-127-7.compute-1.amazonaws.com:~/dump/scripts/"
-#ssh daniel@ec2-184-72-234-254.compute-1.amazonaws.com "ls -l "
-#daniel@ec2-184-72-234-254.compute-1.amazonaws.com
