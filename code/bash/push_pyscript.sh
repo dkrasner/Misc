a927fc4f0af6441d2499abc3d04172b5e0b16456
dkrasner <dkrasner@math.columbia.edu>
2012-04-14 18:44:04 -0300

    Update code/bash/push_pyscript.sh

--- code/bash/push_pyscript.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat $1 | ssh daniel@ec2-184-72-234-254.compute-1.amazonaws.com "cat> $1 ;scp $1 ec2-50-16-142-215.compute-1.amazonaws.com:~/dump/scripts/"
-#ssh daniel@ec2-184-72-234-254.compute-1.amazonaws.com "ls -l "
